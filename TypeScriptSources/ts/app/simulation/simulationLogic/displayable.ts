458efed1f96a40704eafaa66df564107bca7acd7
AetherDim <74829006+AetherDim@users.noreply.github.com>
2020-12-23 20:10:58 +0100

    Helper methods

--- TypeScriptSources/ts/app/simulation/simulationLogic/displayable.ts
+++ TypeScriptSources/ts/app/simulation/simulationLogic/displayable.ts
@@ -1,4 +1,4 @@
-import { Body } from "matter-js";
+import { Body, Bodies, Vertices } from "matter-js";
 
 export class Displayable {
     displayObject: PIXI.DisplayObject = null;
@@ -6,6 +6,13 @@ export class Displayable {
     x: number = 0;
     y: number = 0;
     rotation: number = 0;
+
+    constructor(displayObject: PIXI.DisplayObject, x: number = 0, y: number = 0, rotation: number = 0) {
+        this.displayObject = displayObject;
+        this.x = x;
+        this.y = y;
+        this.rotation = rotation;
+    }
 
     setPosition(x: number, y: number) {
         this.displayObject.x = x-this.x;
@@ -41,3 +48,66 @@ export class Displayable {
     }
 
 }
+
+
+export abstract class DisplaySettings {
+
+    x: number;
+    y: number;
+
+    texture?: PIXI.Texture;
+    color?: number = 0xFFFFFF;
+    alpha?: number = 1;
+    strokeColor?: number = 0x000000;
+    strokeAlpha?: number = 1;
+    strokeWidth?: number = 2;
+
+}
+
+
+export function createRect(settings: DisplaySettings, width: number, height: number, roundingAngle: number=0) {
+
+    const graphics = new PIXI.Graphics();
+
+    graphics.lineStyle(settings.strokeWidth, settings.strokeColor, settings.strokeAlpha);
+    graphics.beginFill(settings.color, settings.alpha);
+    graphics.drawRoundedRect(settings.x, settings.y, width, height, roundingAngle);
+    graphics.endFill();
+
+
+    var displayable = new Displayable(graphics);
+
+    return Bodies.rectangle(settings.x, settings.y, width, height, {displayable: displayable});
+}
+
+export function createCircle(settings: DisplaySettings, radius: number) {
+
+    const graphics = new PIXI.Graphics();
+
+    graphics.lineStyle(settings.strokeWidth, settings.strokeColor, settings.strokeAlpha);
+    graphics.beginFill(settings.color, settings.alpha);
+    graphics.drawCircle(settings.x, settings.y, radius);
+    graphics.endFill();
+
+    var displayable = new Displayable(graphics);
+
+    return Bodies.circle(settings.x, settings.y, radius, {displayable: displayable});
+}
+
+export function createPoligon(settings: DisplaySettings, radius: number) {
+
+    const graphics = new PIXI.Graphics();
+
+    graphics.lineStyle(settings.strokeWidth, settings.strokeColor, settings.strokeAlpha);
+    graphics.beginFill(settings.color, settings.alpha);
+    graphics.drawCircle(settings.x, settings.y, radius);
+    graphics.endFill();
+
+    var displayable = new Displayable(graphics);
+
+
+    //return Bodies.polygon(settings.x, settings.y, width, height, {displayable: displayable});
+
+
+}
+
